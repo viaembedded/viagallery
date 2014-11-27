94ec2d3878d702953fa83735c63c248684ff3d76
Gergely Imreh <imrehg@gmail.com>
2014-11-27 17:26:30 +0800

    add copyright to footer

---
+++ wp-content/themes/viagallery/footer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ */
+?>
+
+		</div><!-- #main -->
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<?php get_sidebar( 'main' ); ?>
+
+			<div class="site-info">
+			        Copyright ©2014  VIA Technologies, Inc., All Rights Reserved.
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<?php wp_footer(); ?>
+</body>
+</html>
