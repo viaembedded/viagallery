c482f9b1cc1c495dbc55cc2d66420a72aa198af0
Gergely Imreh <imrehg@gmail.com>
2014-11-29 18:21:29 +0800

    theme: add customized 404 page with highlighted search

---
+++ wp-content/themes/viagallery/404.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+			<div class="page-wrapper">
+				<div class="page-content">
+					<h2><?php _e( 'It looks like nothing was found at this location.', 'viagallery' ); ?></h2>
+					<p><?php _e( 'We have recently re-engineered the VIA Gallery website for long-term improvement, and the galleries might have moved. Maybe try a search?', 'viagallery' ); ?></p>
+
+					<?php get_search_form(); ?>
+				</div><!-- .page-content -->
+			</div><!-- .page-wrapper -->
+
+			<header class="page-header">
+				<h1 class="page-title"><?php _e( 'Not Found', 'twentythirteen' ); ?></h1>
+			</header>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
